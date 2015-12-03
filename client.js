acc874d2c2bc383a460f90ee76f67554d2e06689
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-12-03 14:22:18 +0700

    Add message definition to handle weird one-way operation on node-soap

--- client.js
+++ client.js
@@ -6,9 +6,10 @@ const soap = require('soap');
 var ca = fs.readFileSync('./CA_Sistem_Terdistribusi_2015.crt');
 https.globalAgent.options.ca = [ca];
 
-soap.createClient('https://nabilati.sisdis.ui.ac.id/ta/wsdl', function (err, client) {
-  console.log(err);
-  client.ping(null, (err, response) => {
+soap.createClient('https://priambodo.sisdis.ui.ac.id/bank/wsdl', function (err, client) {
+  if (err) return console.log(err);
+  client.register({ user_id: '1235', nama: 'test', ip_domisili: 'halo' }, (err, response) => {
+    if (err) return console.log(err);
     console.log(response);
   });
 });
