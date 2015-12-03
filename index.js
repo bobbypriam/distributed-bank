45eba9aaad1968133ae2caa2512d23fc14f6be70
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-12-03 19:01:08 +0700

    Add client UI for testing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const soap = require('soap');
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 // TODO: require mongoose models.
 const models = require('./lib/models');
@@ -13,10 +14,16 @@ const KantorCabangService = { KantorCabang: { KantorCabangPort: services } };
 
 // Express app
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // WSDL specification
 const xml = fs.readFileSync('specification.wsdl', 'utf8');
 app.get('/wsdl', (req, res) => res.set('Content-Type', 'text/xml').send(xml));
+
+// Client app
+const client = require('./lib/client')(models);
+app.use(client);
 
 // Initialize database connection
 mongoose.connect('mongodb://localhost/bank');
@@ -26,6 +33,8 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 8888;
 server.listen(PORT, () => console.log(`Listening at ${PORT}`));
 const soapServer = soap.listen(server, '/', KantorCabangService, xml);
-soapServer.log = function (type, data) {
-  console.log(type, data);
-};
+
+// UNCOMMENT THIS FOR SERVER LOGGING
+// soapServer.log = function (type, data) {
+//   console.log(type, data);
+// };
