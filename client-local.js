45b4cf06ca1dafa6b796543237365f63ad268f3a
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-12-03 19:01:19 +0700

    Updated client-local.js

--- client-local.js
+++ client-local.js
@@ -9,8 +9,28 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
 soap.createClient('https://sisdis.local/wsdl', function (err, client) {
   if (err) return console.log(err);
-  client.register({ user_id: '4321', nama: 'test', ip_domisili: '32323' }, (err, response) => {
+
+  // register
+  // client.register({ user_id: '1206208315', nama: 'Widyanto Bagus Priambodo', ip_domisili: 'https://priambodo.sisdis.ui.ac.id' }, (err, response) => {
+  //   if (err) return console.log(err);
+  //   console.log(response);
+  // });
+
+  // transfer
+  client.transfer({ user_id: '1206208315', nilai: '1000' }, (err, response) => {
     if (err) return console.log(err);
     console.log(response);
+
+    // getSaldo
+    client.getSaldo({ user_id: '1206208315' }, function (err, response) {
+      if (err) return console.log(err);
+      console.log(response);
+    });
   });
+
+  // ping
+  // client.ping(null, function (err, response) {
+  //   if (err) return console.log(err);
+  //   console.log(response);
+  // });
 });
